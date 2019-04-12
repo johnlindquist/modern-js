190eb14a5d653ecfaee991760117e161e385d729
John Lindquist <johnlindquist@gmail.com>
2019-04-12 15:57:47 -0600

    Fri Apr 12 15:57:47 MDT 2019

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-const fruits = ["apple", "orange"]
-const veggies = ["celery", "carrots"]
+const fruit = ["apple", "orange", "banana"]
 
-const createPerson = name => ({
-  name
-})
+const shop = ([first, ...rest]) => {
+  return [first, true]
+}
 
-// const createPerson = name => ({
-//   name
-// })
+const [firstShoppingItem, bought] = shop(fruit)
 
-console.log(createPerson("John"))
-
-const food = [
-  ...fruits.map(name => ({
-    name,
-    type: "fruit"
-  })),
-
-  ...veggies.map(name => ({
-    name,
-    type: "veggie"
-  }))
-]
-
-console.log(food)
+console.log(firstShoppingItem, bought)
