c39ef42faa56f6df4c5e88b11af171cfd19e59c6
John Lindquist <johnlindquist@gmail.com>
2019-04-12 09:26:16 -0600

    Fri Apr 12 09:26:16 MDT 2019

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
-const message = "hello"
+const greeting = "hi"
+const name = "John"
+
+const div = (strings, greeting, name) => {
+  const trimmedString = strings.map(string => string.trim()).join("")
+
+  return `<div>${greeting} 
+  
+  
+  ${trimmedString}
+
+  ${name}
+  </div>`
+}
+
+const message = div`
+${greeting}
+${name}
+
+How are you today?
+`
 
 console.log(message)
